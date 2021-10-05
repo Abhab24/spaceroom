ce40628278bde50d9c27e80aa2ceebf7616482c2
DeekshitaV <deekshitaverma@gmail.com>
2021-10-05 20:58:20 +0530

    Activate Crystal after all clues answered

--- room4/Assets/Scripts/ButtonTrigger.cs
+++ room4/Assets/Scripts/ButtonTrigger.cs
@@ -6,25 +6,30 @@ using UnityEngine.UI;
 public class ButtonTrigger : MonoBehaviour
 {
     public InputField answer;
-
+    static int count = 0;
     public MouseLook mouseLook;
     public AudioSource Correct;
     public AudioSource Incorrect;
     public GameObject Collider;
     public GameObject popup;
+    public GameObject stone;
     string[] actualAns = { "144", "190", "114", "7650" };
 
     public void onSubmit(int i)
     {
         if (answer.text.ToUpper() == actualAns[i].ToUpper())
         {
-            
+            count++;
             mouseLook.enabled = true;
             Debug.Log("You did it!");
             //  MyAnimationController.SetBool("open", true);
             Correct.Play();
             Collider.SetActive(false);
             popup.SetActive(false);
+            if(count == 4)
+            {
+                stone.SetActive(true);
+            }
         }
         else
         {
@@ -33,6 +38,7 @@ public class ButtonTrigger : MonoBehaviour
         }
     }
 
+    
     
 
 
